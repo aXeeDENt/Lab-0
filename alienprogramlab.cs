69a3708ffcefe30de39ebcb785ebdffd0928e6d4
Tatarincev Denis <denis.tatrintev@isa.utm.md>
2024-10-21 02:43:11 +0300

    Successfully finishes the sorting algorithm

--- alienprogramlab.cs
+++ alienprogramlab.cs
@@ -1,13 +1,18 @@
+using System.Collections.Generic;
+
 namespace Lab_0
 {
     public class AlienClass
     {
-        public int? Id { get; set; }
-        public bool? IsHuman { get; set; }
-        public string? Planet { get; set; }
-        public int? Age { get; set; }
-        public List<string>? Traits { get; set; }
+        public int id { get; set; }
+        public bool? isHumanoid { get; set; }
+        public string? planet { get; set; }
+        public int? age { get; set; }
+        public List<string>? traits { get; set; }
+    }
 
-
+    public class AlienData
+    {
+        public List<AlienClass>? input { get; set; }
     }
 }
